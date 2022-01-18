aa3d51b549bcb39281edacfe57e475a1e42bfe10
Amy <amaguire@AJMs-MBP.hsd1.ma.comcast.net>
2022-01-17 20:16:52 -0500

    new instructions added for establish PAT for communication between RStudio and GitHub

--- code.R
+++ code.R
@@ -1,6 +1,14 @@
 library(tidyverse)
+
+To install a personal access token
+install.packages("usethis")
+library(usethis)
+create_github_token()
+# this sends you to a webpage. Don't lose your token. 
+
 install.packages("gitcreds")
 library(gitcreds)
 gitcreds_set()
+# Now enter your token
 
-install.packages("usethis")
+# Now you should be able to follow instructions to Push changes to GitHub
